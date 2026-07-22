476a47d3f9c8a2751187bed5b984b9aa497f70c9
Husnain Ali <Husnainab2@gmail.com>
2026-07-22 10:35:46 +0500

    Implement task addition and display functionality with delete and done options

--- TO-DO-List.js
+++ TO-DO-List.js
@@ -2,3 +2,41 @@ let TaskEnt=document.querySelector('.add-task');
 let AddBtn = document.querySelector('.add-btn');
 let TaskList = document.querySelector('.task-list');
 
+let tasks = [];
+
+AddBtn.addEventListener('click', function() {
+    tasks.push(TaskEnt.value);
+    TaskEnt.value = '';
+    DisplayTasks();
+});
+
+function DisplayTasks() {
+    TaskList.innerHTML = '';
+    tasks.forEach(function(task, index) {
+        let taskitem = document.createElement('li');
+        taskitem.classList.add('task-item');
+        taskitem.innerHTML = `
+            <span class="task-text">${task}</span>
+            <button class="delete-btn" data-index="${index}">Delete</button>
+            <button class="done-btn" data-index="${index}">Done</button>
+        `;
+        TaskList.appendChild(taskitem);
+    });
+
+    let deleteButtons = document.querySelectorAll('.delete-btn');
+    deleteButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            let index = this.getAttribute('data-index');
+            tasks.splice(index, 1);
+            DisplayTasks();
+        });
+    });
+    
+    let doneButtons = document.querySelectorAll('.done-btn');
+    doneButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            let taskItem = this.closest('li');
+            taskItem.style.textDecoration = 'line-through';
+        });
+    });
+}       
